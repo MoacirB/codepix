d06eeac8384bdc1e9c02783c630dab49c07f9cd1
MoacirB <moacir-batista-tavares@hotmail.com>
2021-02-09 19:11:13 -0300

    correct account_test

--- domain/model/account_test.go
+++ domain/model/account_test.go
@@ -2,34 +2,6 @@ package model_test
 
 import (
 	"testing"
-	uuid "github.com/satori/go.uuid"
-	"github.com/codeedu/imersao/codepix-go/domain/model"
-	"github.com/stretchr/testify/require"
-)
-
-func TestModel_NewAccount(t *testing.T) {
-	code := "001"
-	name := "Banco do Brasil"
-	bank, err := model.NewBank(code, name)
-
-	accountNumber := "abcnumber"
-	ownerName := "Wesley"
-	account, err := model.NewAccount(bank, accountNumber, ownerName)
-
-	require.Nil(t, err)
-	require.NotEmpty(t, uuid.FromStringOrNil(account.ID))
-	require.Equal(t, account.Number, accountNumber)
-	require.Equal(t, account.BankID, bank.ID)
-
-	_, err = model.NewAccount(bank, "", ownerName)
-	require.NotNil(t, err)
-=======
-=======
->>>>>>> 3a9907998b9e846343bed63daaab47c0499eda5c
-package model_test
-
-import (
-	"testing"
 
 	uuid "github.com/satori/go.uuid"
 
@@ -53,8 +25,4 @@ func TestModel_NewAccount(t *testing.T) {
 
 	_, err = model.NewAccount(bank, "", ownerName)
 	require.NotNil(t, err)
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> 3a9907998b9e846343bed63daaab47c0499eda5c
 }
